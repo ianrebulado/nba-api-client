f3702f12c7f3a0db7e85fccd6b4202eed6f98d46
ianrebulado <eilrebulado@gmail.com>
2024-05-08 22:33:45 +0800

    chore: change get player params to name

--- app/clients/nba_wrapper.rb
+++ app/clients/nba_wrapper.rb
@@ -3,22 +3,21 @@ require 'faraday'
 class NbaWrapper
   def initialize
     @conn = Faraday.new(url: 'https://api-nba-v1.p.rapidapi.com')
-    @conn.headers['x-rapidapi-host'] = 'api-nba-v1.p.rapidapi.com'
     @conn.headers['x-rapidapi-key'] = '4d404ba3bamsh92c04058ca2f5acp1fabd4jsn7302a145e891'
   end
 
   def get_team(name)
-    response = @conn.get('/teams', {name: name})
+    response = @conn.get('/teams', { name: name })
     parse_response(response)
   end
 
-  def get_player(id)
-    response = @conn.get("/players", {id: id})
+  def get_player(name)
+    response = @conn.get('/players', { name: name })
     parse_response(response)
   end
 
   def live_games
-    response = @conn.get("/games", {live: "all"})
+    response = @conn.get('/games', { live: 'all' })
     parse_response(response)
   end
 
@@ -26,7 +25,6 @@ class NbaWrapper
 
   def parse_response(response)
     raise "#{response.status}" unless response.success?
-
     JSON.parse(response.body)
   end
 end
